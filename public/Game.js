2fd2b063c1bc4a5f5d239b8294e280ef5352889e
ekamil <ekami@LAPTOP-LFC59QR3>
2022-11-09 23:59:29 -0500

    Added background and sprite for game logic

--- public/Game.js
+++ public/Game.js
@@ -1,8 +1,71 @@
-const map = () => {
+const draw = (option) => {
+    var images;
+    var canvas;
+
+    if (option === "main" || option =="loading"){
+        images = {
+            background: '/Images/map/country-platform-back.png',
+        };
+
+        canvas = addImages(images);
+    }
+
+    else if (option === "game"){
+        images = {
+            background: '/Images/map/country-platform-back.png',
+            tiles: '/Images/map/country-platform-tiles-example.png',
+            idle_left: '/Images/character_left/Cowboy4_idle without gun_2.png',
+            idle_right: '/Images/character_right/Cowboy4_idle without gun_2.png'
+        };
+
+        canvas = addImages(images);
+    }
+
+    return canvas;
+}
+
+const addImages = (images) => {
     const canvas = document.createElement("CANVAS");
     const ctx = canvas.getContext("2d");
-    ctx.fillStyle = "#FF0000";
-    ctx.fillRect(0,0,150,75);
-    
-    return canvas;
+      
+      for(let key in images){
+        let image = new Image();
+        image.src = images[key];
+        image.onload = function(){
+          images[key] = image;
+          draw();
+        };
+      }
+      
+      var drawOrder = [ 
+        {id: 'background', position: {x: 0, y: 0}},
+        {id: 'tiles', position: {x: -75, y: -65}},
+        {id: 'idle_left', position: {x:120,y: 90, w:40, h:40}},
+        {id: 'idle_right', position: {x:140,y: 90, w:40, h:40}},
+      ];
+      
+      ctx.imageSmoothingEnabled = true;
+      ctx.imageSmoothingQuality = 'high'; 
+      
+      var current = 0;
+
+      var draw = function(){
+      
+        var step = drawOrder[current];
+        if(typeof images[step.id] === 'string') return ; // not loaded yet
+        console.log(step)
+        if(!Array.isArray(step.position)) step.position = [step.position];
+      
+        step.position.forEach(pos => 
+          'w' in pos
+          ? ctx.drawImage(images[step.id], pos.x, pos.y, pos.w, pos.h)
+          : ctx.drawImage(images[step.id], pos.x, pos.y)
+        );
+      
+        current++;
+        if(current < drawOrder.length)
+          draw();
+      };
+
+      return canvas;
 }
