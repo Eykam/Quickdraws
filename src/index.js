c9f7c989aca691f73503b4d7f18bb52c599f6574
ekamil <ekami@LAPTOP-LFC59QR3>
2022-11-10 01:29:27 -0500

    Added feature to display usernames in game state

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ io.on("connection", (socket) => {
     //Initial emits when new user connects
     socket.emit("clientID", clientID);
     
-    clients[clientID] = {
+    clients[socket.id] = {
         "clientID" : clientID,
         "username" : "",
         "room" : ""
@@ -33,11 +33,11 @@ io.on("connection", (socket) => {
 
     //Server messages
     socket.on("disconnect", () => {
-        console.log("Room to leave: ",clients[clientID]['room'])
+        console.log("Room to leave: ",clients[socket.id]['room'])
         socket.emit("leftGame");
-        io.socketsLeave(clients[clientID]['room']);
+        io.socketsLeave(clients[socket.id]['room']);
         console.log("User disconnected: ", clientID);
-        delete clients[clientID];
+        delete clients[socket.id];
     })
 
     socket.on("joinRoom", (data) => {
@@ -50,20 +50,19 @@ io.on("connection", (socket) => {
         let foundRoom = false;
         let clientRoom = "";
         
-        clients[clientID]['username'] = data;
+        clients[socket.id]['username'] = data;
         
         for (let x = 0; x < roomIDs.length; x++){
             let currRoom = roomIDs[x];
 
-            if(!currRoom.includes("room")){
-                console.log("Doesnt contain room");
+            if(!currRoom.includes("room"))
                 continue;
-            }
+
 
             if([...rooms.get(currRoom)].length < 2){
                 socket.join(currRoom);
                 clientRoom = currRoom;
-                clients[clientID]['room'] = currRoom;
+                clients[socket.id]['room'] = currRoom;
                 foundRoom = true;
             }
             
@@ -73,9 +72,32 @@ io.on("connection", (socket) => {
             socket.join("room-" + guid());
             clientRoom = "room-" + guid();
         }
+        
+        if(foundRoom){
+            console.log("Current Socket: ", socket.id)
+            console.log("Found Room! ", [...rooms.get(clientRoom)]);
+            let sockets = [...rooms.get(clientRoom)];
+            let payload = {};
+            let player1 = {};
+            let player2 = {};
 
-        console.log("Found Room! ", clientRoom);
-        io.to(clientRoom).emit("foundRoom");
+          
+            player1['clientID'] = clients[sockets[0]]['clientID'];
+            player1['username'] = clients[sockets[0]]['username'];
+
+            player2['clientID'] = clients[sockets[1]]['clientID'];
+            player2['username'] = clients[sockets[1]]['username'];
+
+
+            payload["player1"] = player1;
+            payload["player2"] = player2;
+
+
+            console.log("Current Socket: ",socket.id);
+            console.log("Payload: ", payload);
+            
+            io.to(clientRoom).emit("foundRoom", payload);
+        }
     })
 
     //Clean-up / server-side logic
