cfb90f1d0d03a0a0b6d195ef7778ee1c84aa1e1b
ekamil <ekami@LAPTOP-LFC59QR3>
2022-11-18 17:40:59 -0500

    Added birds

--- public/GameLogic/Game.js
+++ public/GameLogic/Game.js
@@ -6,6 +6,8 @@ const charWidth = "150px";
 const startPosRight = "46%";
 const startPosLeft = "40%";
 const deadSpritePos = "82%";
+const walkSpeed = 150;
+const walkDistance = 1 / 2;
 let currSprite = 0;
 let baseDirLeft = "../Images/character_left/";
 let baseDirRight = "../Images/character_right/";
@@ -16,83 +18,73 @@ let baseDead = "Cowboy4_dead.png";
 let baseShoot = "Cowboy4_shoot_1.png";
 let walkInterval = "";
 
-
 function startPosition(parent) {
+  leftPlayerCharacter.src = baseDirLeft + baseIdle;
+  leftPlayerCharacter.style.position = "fixed";
+  leftPlayerCharacter.style.left = startPosLeft;
+  leftPlayerCharacter.style.top = topPos;
+  leftPlayerCharacter.style.width = charWidth;
+  leftPlayerCharacter.style.height = charHeight;
+  leftPlayerCharacter.style.display = "block";
+  leftPlayerCharacter.id = "left-player-char";
 
-    leftPlayerCharacter.src = baseDirLeft + baseIdle;
-    leftPlayerCharacter.style.position = "fixed";
-    leftPlayerCharacter.style.left = startPosLeft;
-    leftPlayerCharacter.style.top = topPos;
-    leftPlayerCharacter.style.width =charWidth;
-    leftPlayerCharacter.style.height =charHeight;
-    leftPlayerCharacter.style.display ="block";
-    leftPlayerCharacter.id="left-player-char";
+  rightPlayerCharacter.src = baseDirRight + baseIdle;
+  rightPlayerCharacter.style.position = "fixed";
+  rightPlayerCharacter.style.left = startPosRight;
+  rightPlayerCharacter.style.top = topPos;
+  rightPlayerCharacter.style.width = charWidth;
+  rightPlayerCharacter.style.height = charHeight;
+  rightPlayerCharacter.style.display = "block";
+  rightPlayerCharacter.id = "right-player-char";
 
-
-    rightPlayerCharacter.src = baseDirRight + baseIdle;
-    rightPlayerCharacter.style.position = "fixed";
-    rightPlayerCharacter.style.left = startPosRight;
-    rightPlayerCharacter.style.top = topPos;
-    rightPlayerCharacter.style.width = charWidth;
-    rightPlayerCharacter.style.height = charHeight;
-    rightPlayerCharacter.style.display ="block";
-    rightPlayerCharacter.id="right-player-char";
-
-    parent.appendChild(leftPlayerCharacter);
-    parent.appendChild(rightPlayerCharacter);
-
+  parent.appendChild(leftPlayerCharacter);
+  parent.appendChild(rightPlayerCharacter);
 }
-
 
 let walk = () => {
-    let srcNum = currSprite % 4;
-    let imgRight = baseDirRight + baseWalkImg + srcNum + ".png"
-    let imgLeft = baseDirLeft + baseWalkImg + srcNum + ".png"
-    let currCoordRight = parseFloat(rightPlayerCharacter.style.left);
-    let currCoordLeft = parseFloat(leftPlayerCharacter.style.left);
+  let srcNum = currSprite % 4;
+  let imgRight = baseDirRight + baseWalkImg + srcNum + ".png";
+  let imgLeft = baseDirLeft + baseWalkImg + srcNum + ".png";
+  let currCoordRight = parseFloat(rightPlayerCharacter.style.left);
+  let currCoordLeft = parseFloat(leftPlayerCharacter.style.left);
 
+  rightPlayerCharacter.src = imgRight;
+  currCoordRight = currCoordRight + walkDistance;
+  rightPlayerCharacter.style.left = currCoordRight + "%";
 
-    rightPlayerCharacter.src = imgRight;
-    currCoordRight = currCoordRight + 1;
-    rightPlayerCharacter.style.left = currCoordRight + "%"; 
+  leftPlayerCharacter.src = imgLeft;
+  currCoordLeft = currCoordLeft - walkDistance;
+  leftPlayerCharacter.style.left = currCoordLeft + "%";
 
-    leftPlayerCharacter.src = imgLeft;
-    currCoordLeft = currCoordLeft - 1;
-    leftPlayerCharacter.style.left = currCoordLeft + "%";
+  currSprite++;
+};
 
-    currSprite++;
-}
-
-function walking(){
-    clearInterval(walkInterval);
-    walkInterval = setInterval(walk, 300);
+function walking() {
+  clearInterval(walkInterval);
+  walkInterval = setInterval(walk, walkSpeed);
 }
 
 // function shooting(){
 //     clearInterval(walkInterval);
 // }
 
-function setShot(direction){
-
-    if(direction == "right")
-        rightPlayerCharacter.src = baseDirRight + baseShoot;
-    else
-        leftPlayerCharacter.src = baseDirLeft + baseShoot;
+function setShot(direction) {
+  if (direction == "right") rightPlayerCharacter.src = baseDirRight + baseShoot;
+  else leftPlayerCharacter.src = baseDirLeft + baseShoot;
 }
 
-function setDead(direction){
-    if(direction == "right"){
-        rightPlayerCharacter.src = baseDirRight + baseDead;
-        rightPlayerCharacter.style.top = deadSpritePos; 
-    }
-    else{
-        leftPlayerCharacter.src = baseDirLeft + baseDead;
-        leftPlayerCharacter.style.top = deadSpritePos;
-    }
+function setDead(direction) {
+  if (direction == "right") {
+    rightPlayerCharacter.src = baseDirRight + baseDead;
+    rightPlayerCharacter.style.top = deadSpritePos;
+  } else {
+    leftPlayerCharacter.src = baseDirLeft + baseDead;
+    leftPlayerCharacter.style.top = deadSpritePos;
+  }
 }
 
-function turn(){
-    clearInterval(walkInterval);
-    leftPlayerCharacter.src = baseDirLeft + baseTurn;
-    rightPlayerCharacter.src = baseDirRight + baseTurn;
+function turn() {
+  clearInterval(walkInterval);
+  leftPlayerCharacter.src = baseDirLeft + baseTurn;
+  rightPlayerCharacter.src = baseDirRight + baseTurn;
 }
--- public/GameLogic/Main.js
+++ public/GameLogic/Main.js
@@ -5,6 +5,7 @@ const rightPlayerInfo = document.getElementById("right-player");
 const gameInfo = document.getElementById("game-info");
 const winnerInfo = document.getElementById("winner-info");
 
+const loader = document.createElement("div");
 const div_loading = document.createElement("div");
 const leftPlayerLives = document.createElement("div");
 const rightPlayerLives = document.createElement("div");
@@ -12,7 +13,6 @@ const BASE_TIMER = 3000;
 
 //change back to main
 let canvas = draw("game");
-
 
 const socket = io();
 
@@ -22,255 +22,246 @@ let playerUsername = "";
 
 //Render Background for start menu
 outerBox.appendChild(canvas);
-winnerInfo.innerText="";
+outerBox.appendChild(createBirds());
+winnerInfo.innerText = "";
 winnerInfo.classList.add("winner-info");
-
 
 //Receiving messages from server
 socket.on("clientID", (message) => {
-    clientID = message;
-})
+  clientID = message;
+});
 
 socket.on("foundRoom", (data) => {
-    let player1ID = data["player1"]["clientID"];
-    let totalLives = Math.floor(data["totalRounds"] / 2) + 1;
-    let leftPlayer = {};
-    let rightPlayer = {};
-    
+  let player1ID = data["player1"]["clientID"];
+  let totalLives = Math.floor(data["totalRounds"] / 2) + 1;
+  let leftPlayer = {};
+  let rightPlayer = {};
 
-    div_loading.remove();
-    canvas.remove();
-    canvas = draw("game");
-    outerBox.appendChild(canvas);
-    
-    
+  div_loading.remove();
+  canvas.remove();
+  canvas = draw("game");
+  outerBox.appendChild(canvas);
 
-    if(player1ID === clientID){
+  if (player1ID === clientID) {
+    leftPlayer = data["player1"];
+    rightPlayer = data["player2"];
+  } else {
+    leftPlayer = data["player2"];
+    rightPlayer = data["player1"];
+  }
 
-        leftPlayer = data["player1"]
-        rightPlayer = data["player2"]
-    
-    }
-    else{
+  leftPlayerInfo.style.display = "block";
+  rightPlayerInfo.style.display = "block";
 
-        leftPlayer = data["player2"]
-        rightPlayer = data["player1"]
-    }
+  leftPlayerInfo.innerText =
+    leftPlayer["username"] + "#" + leftPlayer["clientID"];
+  rightPlayerInfo.innerText =
+    rightPlayer["username"] + "#" + rightPlayer["clientID"];
 
-    leftPlayerInfo.style.display="block";
-    rightPlayerInfo.style.display="block";
+  leftPlayerLives.classList.add("lives");
+  leftPlayerLives.innerText = "Lives: ";
+  leftPlayerInfo.appendChild(
+    renderLives(leftPlayerLives, leftPlayer["lives"], totalLives)
+  );
 
-    leftPlayerInfo.innerText = leftPlayer['username'] + "#" + leftPlayer['clientID'];
-    rightPlayerInfo.innerText = rightPlayer['username'] + "#" + rightPlayer['clientID'];
+  rightPlayerLives.classList.add("lives");
+  rightPlayerLives.innerText = "Lives: ";
+  rightPlayerInfo.appendChild(
+    renderLives(rightPlayerLives, rightPlayer["lives"], totalLives)
+  );
 
-    leftPlayerLives.classList.add("lives")
-    leftPlayerLives.innerText = "Lives: "
-    leftPlayerInfo.appendChild(renderLives(leftPlayerLives, leftPlayer["lives"], totalLives));
+  //inital render of current round and countdown
+  gameInfo.appendChild(roundDisplay(data["currRound"], gameInfo));
+  gameInfo.appendChild(timer(BASE_TIMER, gameInfo));
 
-    rightPlayerLives.classList.add("lives")
-    rightPlayerLives.innerText = "Lives: "
-    rightPlayerInfo.appendChild(renderLives(rightPlayerLives, rightPlayer["lives"], totalLives));
-
-
-    //inital render of current round and countdown
-    gameInfo.appendChild(roundDisplay(data["currRound"],gameInfo));
-    gameInfo.appendChild(timer(BASE_TIMER, gameInfo));
-
-    //Add moving logic here
-    startPosition(outerBox);
-    walking();
-    socket.emit("gameReady")
-
-})
-
+  //Add moving logic here
+  startPosition(outerBox);
+  walking();
+  socket.emit("gameReady");
+});
 
 socket.on("startGame", (message) => {
-    //Add turn logic here
-    turn();
-    console.log(message)
-    document.addEventListener("click", shoot);
-    document.addEventListener("keypress", shoot);
-
+  //Add turn logic here
+  turn();
+  console.log(message);
+  document.addEventListener("click", shoot);
+  document.addEventListener("keypress", shoot);
 });
-
 
 socket.on("roundWinner", (data) => {
-    let leftPlayer = "";
-    let rightPlayer = "";
-    let player1ID = data["player1"]["clientID"];
-    let totalLives = Math.floor(data["totalRounds"] / 2) + 1;
+  let leftPlayer = "";
+  let rightPlayer = "";
+  let player1ID = data["player1"]["clientID"];
+  let totalLives = Math.floor(data["totalRounds"] / 2) + 1;
 
-    if(player1ID === clientID){
+  if (player1ID === clientID) {
+    leftPlayer = data["player1"];
+    rightPlayer = data["player2"];
+  } else {
+    leftPlayer = data["player2"];
+    rightPlayer = data["player1"];
+  }
 
-        leftPlayer = data["player1"]
-        rightPlayer = data["player2"]
+  //redraw canvas to move players back to starting point
+  canvas.remove();
+  canvas = draw("game");
+  outerBox.appendChild(canvas);
+  document.removeEventListener("click", shoot);
+  document.removeEventListener("keypress", shoot);
 
-    }
-    
-    else{
+  //after round, update players info
+  if (data["roundWinner"] === leftPlayer["clientID"]) {
+    setShot("left");
+    setDead("right");
+    winnerInfo.innerText =
+      leftPlayer["username"] + "#" + data["roundWinner"] + " won this round!";
+  } else {
+    setShot("right");
+    setDead("left");
+    winnerInfo.innerText =
+      rightPlayer["username"] + "#" + data["roundWinner"] + " won this round!";
+  }
 
-        leftPlayer = data["player2"]
-        rightPlayer = data["player1"]
-    }
+  leftPlayerInfo.append(
+    renderLives(leftPlayerLives, leftPlayer["lives"], totalLives)
+  );
+  rightPlayerInfo.append(
+    renderLives(rightPlayerLives, rightPlayer["lives"], totalLives)
+  );
+  console.log(
+    "Round " + data["currRound"] + " Winner: ",
+    data["roundWinner"] + "#" + clientID
+  );
 
-    //redraw canvas to move players back to starting point
-    canvas.remove();
-    canvas = draw("game");
-    outerBox.appendChild(canvas);
-    document.removeEventListener("click", shoot);
-    document.removeEventListener("keypress", shoot);
+  //Render component that shows who won the round
+  winnerInfo.style.display = "block";
 
-
-    //after round, update players info
-    if(data['roundWinner'] === leftPlayer['clientID']){
-        setShot("left");
-        setDead("right");
-        winnerInfo.innerText = leftPlayer['username'] + "#" + data['roundWinner'] + " won this round!";
-    }
-    else{
-        setShot("right");
-        setDead("left");
-        winnerInfo.innerText =rightPlayer['username'] + "#" + data['roundWinner'] + " won this round!";
-    }
-
-    leftPlayerInfo.append(renderLives(leftPlayerLives, leftPlayer["lives"], totalLives));
-    rightPlayerInfo.append(renderLives(rightPlayerLives, rightPlayer["lives"], totalLives));
-    console.log("Round " + data["currRound"] + " Winner: ", data["roundWinner"]+ "#" +clientID);
-
-    //Render component that shows who won the round
-    winnerInfo.style.display = "block";
-
-    setTimeout(()=> {
-        socket.emit("nextRound");
-    },4000);
-
+  setTimeout(() => {
+    socket.emit("nextRound");
+  }, 4000);
 });
 
-socket.on("continue", (data)=> {
-    socket.emit("gameReady");
+socket.on("continue", (data) => {
+  socket.emit("gameReady");
 
-    //Add moving logic here
-    startPosition(outerBox);
-    walking();
+  //Add moving logic here
+  startPosition(outerBox);
+  walking();
 
-    //Render current round in top middle and 
-    // countdown until next round starts, after countdown finishes, remove element
-    winnerInfo.style.display = "none"
-    gameInfo.appendChild(roundDisplay(data["currRound"],gameInfo));
-    gameInfo.appendChild(timer(BASE_TIMER, gameInfo));
-    
-})
+  //Render current round in top middle and
+  // countdown until next round starts, after countdown finishes, remove element
+  winnerInfo.style.display = "none";
+  gameInfo.appendChild(roundDisplay(data["currRound"], gameInfo));
+  gameInfo.appendChild(timer(BASE_TIMER, gameInfo));
+});
 
 socket.on("gameWinner", (message) => {
-    const mainmenu = document.createElement("div");
-    document.removeEventListener("click", shoot)
-    document.removeEventListener("keypress", shoot);
+  const mainmenu = document.createElement("div");
+  document.removeEventListener("click", shoot);
+  document.removeEventListener("keypress", shoot);
 
-    //Set shooting and dead sprite
-    if(message === playerUsername + "#" + clientID){
-        setShot("left");
-        setDead("right");
-    }
-    else{
-        setShot("right");
-        setDead("left");
-    }
+  //Set shooting and dead sprite
+  if (message === playerUsername + "#" + clientID) {
+    setShot("left");
+    setDead("right");
+  } else {
+    setShot("right");
+    setDead("left");
+  }
 
-    //Render page that shows Winner if won and Loser if Lost, then reloads page on "go home" button
-    setTimeout(()=>{
-        gameInfo.innerHTML= "";
-        document.getElementById("right-player-char").style.display="none";
-        document.getElementById("left-player-char").style.display="none";
+  //Render page that shows Winner if won and Loser if Lost, then reloads page on "go home" button
+  setTimeout(() => {
+    gameInfo.innerHTML = "";
+    document.getElementById("right-player-char").style.display = "none";
+    document.getElementById("left-player-char").style.display = "none";
 
-        canvas.remove();
-        canvas = draw("main");
-        outerBox.appendChild(canvas);
+    canvas.remove();
+    canvas = draw("main");
+    outerBox.appendChild(canvas);
 
-        mainmenu.innerText = "Click To Go To Main Menu";
-        winnerInfo.innerText = message + " won the Game!";
-        winnerInfo.style.display = "block";
-        winnerInfo.appendChild(mainmenu);
+    mainmenu.innerText = "Click To Go To Main Menu";
+    winnerInfo.innerText = message + " won the Game!";
+    winnerInfo.style.display = "block";
+    winnerInfo.appendChild(mainmenu);
 
-        document.addEventListener("click",()=>{
-            window.location.reload();
-        });
+    document.addEventListener("click", () => {
+      window.location.reload();
+    });
 
-        document.addEventListener("keypress", ()=>{
-            window.location.reload();
-        });
-
-    },3000)
-})
+    document.addEventListener("keypress", () => {
+      window.location.reload();
+    });
+  }, 3000);
+});
 
 //Wiring callback events to send server messages
 usernameForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-    usernameForm.remove();
-    playerUsername = e.target[0].value;
-    
-    div_loading.innerHTML = "Looking for Game...";
-    div_loading.id = "loading"
+  e.preventDefault();
+  usernameForm.remove();
+  playerUsername = e.target[0].value;
 
-    outerBox.appendChild(div_loading);
-    outerBox.appendChild(canvas);
+  div_loading.innerHTML = "Looking for Game...";
+  div_loading.id = "loading";
+  loader.classList.add("loader");
+  //   loader.innerText = "hello";
+  outerBox.appendChild(canvas);
+  outerBox.appendChild(div_loading);
+  div_loading.appendChild(loader);
 
-    socket.emit("joinRoom", e.target[0].value);
-    console.log("User: " + clientID + " attempting to join room")
-})
+  socket.emit("joinRoom", e.target[0].value);
+  console.log("User: " + clientID + " attempting to join room");
+});
 
 //Helper Functions:
-function renderLives(parent, lives, totalLives){
-    parent.innerHTML = "";
+function renderLives(parent, lives, totalLives) {
+  parent.innerHTML = "";
 
-    for (let i= 0; i < totalLives; i++){
-        let currDiv = document.createElement("div");
+  for (let i = 0; i < totalLives; i++) {
+    let currDiv = document.createElement("div");
 
-        if(i < lives) 
-            currDiv.classList.add("remainingLife")
-        else
-            currDiv.classList.add("lostLife")
+    if (i < lives) currDiv.classList.add("remainingLife");
+    else currDiv.classList.add("lostLife");
 
-        parent.appendChild(currDiv)
-    }
+    parent.appendChild(currDiv);
+  }
 
-    return parent;
+  return parent;
 }
 
-const shoot = function (){
-    //Add shoot sprite logic here
-    socket.emit("shoot");
-    console.log("Shooting")
+const shoot = function () {
+  //Add shoot sprite logic here
+  socket.emit("shoot");
+  console.log("Shooting");
 };
 
 const timer = (BASE_TIMER, parent) => {
-    let timeDisplay = document.createElement("div");
-    timeDisplay.classList.add("time-display");
-    let currTime = BASE_TIMER;
+  let timeDisplay = document.createElement("div");
+  timeDisplay.classList.add("time-display");
+  let currTime = BASE_TIMER;
 
-    parent.style.display= "block"
+  parent.style.display = "block";
 
-    let setTime = setInterval(()=>{
-        timeDisplay.innerText = "";
-        if(currTime <= 0){
-            clearInterval(setTime);
-            timeDisplay.style.display="none";
-        }
-        
-        console.log("Time: ", currTime/1000);
-        timeDisplay.innerText= currTime/1000;
-        currTime -= 1000;
+  let setTime = setInterval(() => {
+    timeDisplay.innerText = "";
+    if (currTime <= 0) {
+      clearInterval(setTime);
+      timeDisplay.style.display = "none";
+    }
 
-    },1000);
+    console.log("Time: ", currTime / 1000);
+    timeDisplay.innerText = currTime / 1000;
+    currTime -= 1000;
+  }, 1000);
 
-    return timeDisplay;
-}
+  return timeDisplay;
+};
 
 const roundDisplay = (currRound, parent) => {
-    let roundDisplay = document.createElement("div");
-    
-    parent.innerHTML = "";
-    roundDisplay.classList.add("round-display");
+  let roundDisplay = document.createElement("div");
 
-    roundDisplay.innerText = "Round: " + currRound;
-    return roundDisplay;
-}
+  parent.innerHTML = "";
+  roundDisplay.classList.add("round-display");
+
+  roundDisplay.innerText = "Round: " + currRound;
+  return roundDisplay;
+};
