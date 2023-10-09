f7d53961541789bbde338b233cc010873d7a10cc
Eyad <e.kam12@yahoo.com>
2023-10-09 16:30:38 -0400

    updated

---
+++ README.txt
@@ -0,0 +1,48 @@
+Quickdraw app
+Name: Eyad Kamil UID: 115776821
+
+
+
+
+Description:
+
+Quickdraw is a reaction based, online multiplayer game where the objective is to win a duel.
+The frontend is written using vanilla HTML, CSS, and JS, with a Node, MongoDB, Express, Socket.io backend.
+Some features of the app include a User database with register / login / logout functionaliy,
+an online matchmaking service, and a global leaderboard for all users.
+
+
+
+
+API's:
+
+An API which was used in the implementation of this app is the "Bad Words API". The way this API is used
+is to filter any profanity from players usernames when they are registering since the usernames are 
+public to other players. 
+
+https://apilayer.com/marketplace/bad_words-api
+
+
+
+
+
+Hosting:
+
+The hosting service used for this project was https://railway.app, which provisions resources and deploys projects
+automatically once linked to a github repo.
+
+Public Link:
+https://quickdraw.up.railway.app/
+
+
+
+
+
+Video of Functionality:
+https://youtu.be/bKM1ZOKiqzs
+
+
+
+
+Link to GitHub Repo:
+https://github.com/Eykam/Quickdraws
--- src/database.js
+++ src/database.js
@@ -1,83 +1,79 @@
-require('dotenv').config();
-const axios = require('axios');
+require("dotenv").config();
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 const MONGO_USER = process.env.MONGO_DB_USERNAME;
 const MONGO_PASS = process.env.MONGO_DB_PASSWORD;
 const MONGO_DB_NAME = process.env.MONGO_DB_NAME;
 const MONGO_COLLECTION = process.env.MONGO_COLLECTION;
-const API_KEY = process.env.API_KEY;
 
-const uri = `mongodb+srv://${MONGO_USER}:${MONGO_PASS}@cluster0.iyuilru.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${MONGO_USER}:${MONGO_PASS}@auth.kr7x4gt.mongodb.net/?retryWrites=true&w=majority`;
 
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
 
 const database = client.db(MONGO_DB_NAME);
 
 const apps = database.collection(MONGO_COLLECTION);
 
-async function registerUser(req){
-    const user = req.user
-    const query = {user: user}
-    const entries = await apps.findOne(query)
-    
-    if(entries != null)
-        return [false, "User Already Exists!"];
+async function registerUser(req) {
+  const user = req.user;
+  const query = { user: user };
+  const entries = await apps.findOne(query);
 
-    const profanityFilter = await axios({
-                                        method:'post',
-                                        url: 'https://api.apilayer.com/bad_words?censor_character=censor_character',
-                                        data: {user},
-                                        headers: {"apikey": API_KEY}
-                                });
+  if (entries != null) return [false, "User Already Exists!"];
 
-    console.log("-------------------profanity found: ", profanityFilter.data.bad_words_total > 0)
-    if(profanityFilter.data.bad_words_total > 0){
-        return [false, "Username Not Valid!"]
+  apps.insertOne({
+    user: req.user,
+    pass: req.pass,
+    wins: 0,
+    loses: 0,
+    "total games": 0,
+  });
+  return [true, "success"];
+}
+
+async function loginUser(req) {
+  const query = { user: req.user };
+  const entries = await apps.findOne(query);
+  let message = "";
+
+  if (entries != null) {
+    if (req.pass === entries.pass) {
+      return [true, "Success!"];
+    } else {
+      message = "Password Incorrect!";
     }
+  } else {
+    message = "User Not Found!";
+  }
 
-    apps.insertOne({
-        "user": req.user,
-        "pass": req.pass,
-        "wins": 0,
-        "loses": 0,
-        "total games":0
-    });
-    return [true, "success"];
+  return [false, message];
 }
 
-async function loginUser(req){
-    const query = {user: req.user};
-    const entries = await apps.findOne(query);
-    let message = ""
+async function addResults(user, won) {
+  const wins = won ? 1 : 0;
+  const loses = won ? 0 : 1;
 
-    if (entries != null){
-        if(req.pass === entries.pass){
-            return [true, "Success!"];
-        }
-        else{
-            message = "Password Incorrect!"
-        }
-    }else{
-        message = "User Not Found!"
-    }
-    
-    return [false, message];
-    
+  console.log(user);
+  apps.updateOne(
+    { user: user },
+    { $inc: { wins: wins, loses: loses, "total games": 1 } }
+  );
 }
 
-async function addResults(user, won){
-    const wins = won ? 1 : 0;
-    const loses = won ? 0: 1;
-    
-    console.log(user);
-    apps.updateOne({user: user}, { $inc: { wins: wins, loses: loses, "total games": 1}});
+async function leaderBoard() {
+  const results = await apps.aggregate([
+    { $sort: { wins: -1 } },
+    { $limit: 10 },
+  ]);
+  console.log("results:", results);
+  return results;
 }
 
-async function leaderBoard(){
-    return await apps.aggregate([ {$sort: {wins: -1}}, {$limit: 10}])
-}
-
-
-module.exports = {registerUser, loginUser, addResults, leaderBoard};
+module.exports = { registerUser, loginUser, addResults, leaderBoard };
